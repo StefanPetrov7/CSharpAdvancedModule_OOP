42bc95ec28806ba636c153894d3152e386d276b7
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-02-05 23:33:18 +0200

    Add files via upload

---
+++ Inheritance/Person/Child.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Person
+{
+    public class Child : Person
+    {
+
+        public Child(string name, int age) : base(name, age)
+        {
+        }
+
+        public override int Age  // Override base behavior to verify if teh age in correct.
+        {
+            get { return base.Age; }
+            set
+            {
+                if (value > 15)
+                {
+                    throw new ArgumentException("Age must be less than 16");
+                }
+
+                base.Age = value;
+            }
+        }
+    }
+}
---
+++ Inheritance/Person/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public virtual int Age  // We use virtual because more verifications will be done in inherited classes.
+        {
+            get { return this.age; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age must be positive");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder output = new StringBuilder();
+            output.Append(string.Format("Name: {0}, Age: {1}", this.name, this.age));
+            return output.ToString();
+        }
+    }
+}
