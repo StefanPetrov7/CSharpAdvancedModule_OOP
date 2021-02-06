98f929d47563e1808ac7d3b026518d5361af0d54
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-02-06 23:31:35 +0200

    Add files via upload

---
+++ Inheritance/Animals/Tomcat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Animals
+{
+    public class Tomcat : Cat
+    {
+        private const string gender = "male";
+
+        public Tomcat(string name, int age, string gender) : base(name, age, gender)
+        {
+        }
+
+        public override string Gender
+        {
+            get { return gender; }
+        }
+
+        public override string ProduceSound()
+        {
+            return "MEOW";
+        }
+    }
+}
