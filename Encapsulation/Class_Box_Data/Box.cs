4c1a1607e91ce442309a666b446abd5c049a9e92
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-02-12 19:04:08 +0200

    Add files via upload

---
+++ Encapsulation/Class_Box_Data/Box.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+
+namespace Class_Box_Data
+{
+    public class Box
+    {
+        private const string INVALIDE_EXCEPTION_MSG = "{0} cannot be zero or negative.";
+
+        private double length;
+
+        private double width;
+
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get { return length; }
+            private set
+            {
+                ValidateSide(value, nameof(Length));
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get { return width; }
+            private set
+            {
+                ValidateSide(value, nameof(Width));
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get { return height; }
+            private set
+            {
+                ValidateSide(value, nameof(Height));
+                height = value;
+            }
+        }
+
+        private void ValidateSide(double value, string param)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException(string.Format(INVALIDE_EXCEPTION_MSG, param));
+            }
+        }
+
+        // Surface Area = 2lw + 2lh + 2wh
+        public double CalculateSurfaceArea() => (2 * Length * Width) + (2 * Length * Height) + (2 * Width * Height);
+
+        //  Lateral Surface Area = 2lh + 2wh
+        public double CalculateLateralSurfaceArea() => (2 * Length * Height) + (2 * Width * Height);
+
+        // Volume = lwh
+        public double CalculateVolume() => Length * Width * Height;
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Surface Area - {this.CalculateSurfaceArea():f2}");
+            sb.AppendLine($"Lateral Surface Area - {this.CalculateLateralSurfaceArea():f2}");
+            sb.AppendLine($"Volume - {this.CalculateVolume():f2}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Encapsulation/Class_Box_Data/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Class_Box_Data
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            try
+            {
+                Box box = new Box(length, width, height);
+                Console.WriteLine(box.ToString());
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
