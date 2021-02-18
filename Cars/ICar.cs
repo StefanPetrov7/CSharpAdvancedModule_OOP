0c0549afaf56fbe1694ed53cae59de3efdeb82c5
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-02-18 23:46:50 +0200

    Add files via upload

---
+++ Cars/ICar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cars
+{
+    public interface ICar
+    {
+
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public string Start();
+
+        public string Stop();
+
+    }
+}
---
+++ Cars/Tesla.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+namespace Cars
+{
+    public class Tesla : ICar, IElectricCar
+    {
+        public Tesla(string model, string color, int battery)
+        {
+            this.Model = model;
+            this.Color = color;
+            this.Battery = battery;
+        }
+
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public int Battery { get; set; }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Color} {this.GetType().Name} {this.Model} with {this.Battery} Batteries");
+            sb.AppendLine(this.Start());
+            sb.AppendLine(this.Stop());
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
