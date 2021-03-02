20d67bc86686e40509b0511144e5cac33a8c3ab1
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-03-03 00:15:11 +0200

    Add files via upload

---
+++ Exception_Handling/05_Convert_ToDouble/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _05_Convert_ToDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double num = Convert.ToDouble(Console.ReadLine());
+                throw new FormatException();
+                throw new InvalidCastException();
+                throw new OverflowException();
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (InvalidCastException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine();
+
+
+            //try
+            //{
+            //    try
+            //    {
+            //        try
+            //        {
+            //            double num = Convert.ToDouble(Console.ReadLine());
+            //            throw new InvalidCastException();
+
+            //        }
+            //        catch (InvalidCastException ex)
+            //        {
+            //            Console.WriteLine(ex.Message);
+            //        }
+
+            //        throw new OverflowException();
+
+            //    }
+            //    catch (OverflowException ex)
+            //    {
+            //        Console.WriteLine(ex.Message);
+            //    }
+
+            //    throw new FormatException();
+
+            //}
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+
+            //Console.ReadLine();
+        }
+    }
+}
---
+++ Exception_Handling/06_Valid_Person/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06_Valid_Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Person> people = new List<Person>();
+
+            try
+            {
+                people.Add(new Person("Stefa", "Petrov", 37));
+
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                people.Add(new Person(string.Empty, "Petrov", 37));
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                people.Add(new Person("Stefa", String.Empty, 37));
+
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                people.Add(new Person("Stefa", "Petrov", -1));
+
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                people.Add(new Person("Stefa", "Petrov", 122));
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            foreach (var person in people)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
