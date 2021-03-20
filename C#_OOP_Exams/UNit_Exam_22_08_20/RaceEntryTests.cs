9c26d38d76036a6126c7fb75d4497d82eb43affa
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-03-20 14:17:03 +0200

    Add files via upload

---
+++ C#_OOP_Exams/UNit_Exam_22_08_20/RaceEntryTests.cs
@@ -0,0 +1,93 @@
+using System;
+using NUnit.Framework;
+
+namespace TheRace.Tests
+{
+    [TestFixture]
+    public class RaceEntryTests
+    {
+        private UnitDriver unitDriver;
+        private UnitCar unitCar;
+        private RaceEntry raceEntry;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.unitCar = new UnitCar("car", 10, 10);
+            this.unitDriver = new UnitDriver("driver", this.unitCar);
+            this.raceEntry = new RaceEntry();
+        }
+
+        [Test]
+        public void TestCarCtor()
+        {
+            Assert.AreEqual("car", this.unitCar.Model);
+            Assert.AreEqual(10, this.unitCar.HorsePower);
+            Assert.AreEqual(10, this.unitCar.CubicCentimeters);
+        }
+
+        [Test]
+        public void TestDriverCtor()
+        {
+            Assert.AreEqual("driver", this.unitDriver.Name);
+            Assert.AreEqual("car", this.unitDriver.Car.Model);
+            Assert.AreEqual(10, this.unitDriver.Car.HorsePower);
+            Assert.AreEqual(10, this.unitDriver.Car.CubicCentimeters);
+        }
+
+        [Test]
+        public void TestDriverCtorException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new UnitDriver(null, this.unitCar));
+        }
+
+        [Test]
+        public void TestRaceEntryCtor()
+        {
+            Assert.AreEqual(0, this.raceEntry.Counter);
+        }
+
+        [Test]
+        public void TestAddNullDriver()
+        {
+            this.unitDriver = null;
+            Assert.Throws<InvalidOperationException>(() => this.raceEntry.AddDriver(this.unitDriver));
+        }
+
+        [Test]
+        public void TestAddSameDriver()
+        {
+            UnitDriver test = new UnitDriver("driver", new UnitCar("CarTest", 10, 10));
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                this.raceEntry.AddDriver(this.unitDriver);
+                this.raceEntry.AddDriver(test);
+            });
+        }
+
+        [Test]
+        public void TestAddDriverSuccessfully()
+        {
+            UnitDriver test = new UnitDriver("driverTest", new UnitCar("CarTest", 10, 10));
+            this.raceEntry.AddDriver(this.unitDriver);
+            this.raceEntry.AddDriver(test);
+            Assert.AreEqual(2, this.raceEntry.Counter);
+        }
+
+        [Test]
+        public void TestCalculateAvgException()
+        {
+            this.raceEntry.AddDriver(this.unitDriver);
+            Assert.Throws<InvalidOperationException>(() => this.raceEntry.CalculateAverageHorsePower());
+        }
+
+        [Test]
+        public void TestCorrectAvgCalculation()
+        {
+            UnitDriver test = new UnitDriver("driverTest", new UnitCar("CarTest", 10, 10));
+            this.raceEntry.AddDriver(this.unitDriver);
+            this.raceEntry.AddDriver(test);
+            Assert.AreEqual(10, this.raceEntry.CalculateAverageHorsePower());
+        }
+    }
+}
