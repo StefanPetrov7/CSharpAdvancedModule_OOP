27fe6e8cd6e61cb90d9c520b6ba8678a03bda386
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2021-03-28 23:30:10 +0300

    Add files via upload

---
+++ C#_OOP_Exams/Computers_UNitTest/Computer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Computers_UNitTest
+{
+    public class Computer
+    {
+        public Computer(string manufacturer, string model, decimal price)
+        {
+            this.Manufacturer = manufacturer;
+            this.Model = model;
+            this.Price = price;
+        }
+
+        public string Manufacturer { get; set; }
+
+        public string Model { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ C#_OOP_Exams/Computers_UNitTest/ComputerManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Computers_UNitTest
+{
+    public class ComputerManager
+    {
+        private const string CanNotBeNullMessage = "Can not be null";
+        private readonly List<Computer> computers;
+
+        public ComputerManager()
+        {
+            this.computers = new List<Computer>();
+        }
+
+        public IReadOnlyCollection<Computer> Computers => this.computers.AsReadOnly();
+
+        public int Count => this.computers.Count;
+
+        public void AddComuter(Computer computer)
+        {
+            this.ValidateNllValue(computer, nameof(computer), CanNotBeNullMessage);
+
+            if (this.computers.Any(c => c.Manufacturer == computer.Manufacturer && c.Model == computer.Model))
+            {
+                throw new ArgumentException("This computer already exists");
+            }
+
+            this.computers.Add(computer);
+        }
+
+        public ICollection<Computer> GetComputerByManufacturer(string manufacturer)
+        {
+            this.ValidateNllValue(manufacturer, nameof(manufacturer), CanNotBeNullMessage);
+            ICollection<Computer> computers = this.Computers.Where(c => c.Manufacturer == manufacturer).ToList();
+            return computers;
+        }
+
+        public Computer RemoveComputer(string manufacturer, string model)
+        {
+            Computer computer = this.GetComputer(manufacturer, model);
+            this.computers.Remove(computer);
+            return computer;
+        }
+
+        public Computer GetComputer(string manufacturer, string model)
+        {
+            this.ValidateNllValue(manufacturer, nameof(manufacturer), CanNotBeNullMessage);
+            this.ValidateNllValue(model, nameof(model), CanNotBeNullMessage);
+
+            Computer computer = this.computers.FirstOrDefault(c => c.Manufacturer == manufacturer && c.Model == model);
+
+            if (computer == null)
+            {
+                throw new ArgumentException("There is no computer with thismanufacturer and model");
+            }
+
+            return computer;
+        }
+
+        private void ValidateNllValue(object variable, string variableName, string extentionMessage)
+        {
+            if (variable == null)
+            {
+                throw new ArgumentException(variableName, extentionMessage);
+            }
+        }
+    }
+}
