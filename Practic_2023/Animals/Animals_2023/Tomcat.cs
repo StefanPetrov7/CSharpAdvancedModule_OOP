4af2ab4042e452561d225c8083c6cf3e68a904ed
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2023-09-26 13:38:26 +0300

    Add files via upload

---
+++ Practic_2023/Animals/Animals_2023/Tomcat.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+
+namespace Animals
+{
+    public class Tomcat : Cat
+    {
+        private const string MALE = "male";
+
+        public Tomcat(string name, int age) : base(name, age, MALE)
+        { }
+
+        public override string ProduceSound()
+        {
+            return "MEOW";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder text = new StringBuilder();
+            text.AppendLine($"{this.GetType().Name}");
+            text.AppendLine($"{this.Name} {this.Age} {this.Gender}");
+            text.AppendLine(this.ProduceSound());
+            return text.ToString().TrimEnd();
+        }
+    }
+}
+
