23e5b16025a3a023573fe6c81399be191ffdd463
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2023-10-17 23:17:46 +0300

    Add files via upload

---
+++ Practic_2023/CommandPattern/Core/CommandInterpreter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private const string COMMAND_POSTFIX = "Command";
+
+        public string Read(string args)
+        {
+            string[] tokens = args.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string commandName = tokens[0];
+            string[] commandArgs = tokens.Skip(1).ToArray();
+
+            Assembly assembly = Assembly.GetCallingAssembly();
+            Type commandType = assembly.GetTypes().FirstOrDefault(x => x.Name.ToLower() == commandName.ToLower());
+
+            if (commandType == null)
+            {
+                throw new ArgumentException("Invalid Type");
+            }
+
+            ICommand commandInstance = ((ICommand)Activator.CreateInstance(commandType, new object[] { }));
+            return commandInstance.Execute(commandArgs);
+        }
+    }
+}
+
---
+++ Practic_2023/CommandPattern/Models/HelloCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CommandPattern.Core.Contracts;
+
+namespace CommandPattern.Models
+{
+    public class HelloCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            return ($"Hello, {string.Join(" ", args)}");
+        }
+    }
+}
+
