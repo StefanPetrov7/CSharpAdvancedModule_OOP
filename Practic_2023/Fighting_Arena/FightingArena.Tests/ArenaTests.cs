baa9bfe18d7c16ad3264db4ca7d69d404f2f93ef
Stefan Petrov <76209920+StefanPetrov7@users.noreply.github.com>
2023-10-20 17:47:32 +0300

    Add files via upload

---
+++ Practic_2023/Fighting_Arena/FightingArena.Tests/ArenaTests.cs
@@ -0,0 +1,90 @@
+﻿namespace FightingArena.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ArenaTests
+    {
+        private Arena arena;
+        private Warrior warriorOne;
+        private Warrior WarriorTwo;
+        private Warrior WarriorThree;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.arena = new Arena();
+            this.warriorOne = new Warrior("One", 100, 100);
+            this.WarriorTwo = new Warrior("Two", 150, 150);
+            this.WarriorThree = new Warrior("Three", 200, 200);
+        }
+
+        [Test]
+        public void Test_Arena_Ctor()
+        {
+            Assert.That(this.arena, Is.Not.Null);
+        }
+
+        [Test]
+        public void Test_Arena_Ctor_Count()
+        {
+            this.arena.Enroll(this.warriorOne);
+            this.arena.Enroll(this.WarriorTwo);
+            Assert.That(this.arena.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Test_Arena_Enroll_Warriors_Throws_Exception_ForSameNames()
+        {
+            Warrior sameName = new Warrior("Test", 50, 50);
+            this.arena.Enroll(sameName);
+            this.arena.Enroll(this.warriorOne);
+            Assert.That(() =>
+            {
+                this.arena.Enroll(sameName);
+            }, Throws.InvalidOperationException.With.Message.EqualTo("Warrior is already enrolled for the fights!"));
+        }
+
+        [Test]
+        [TestCase("MissingName")]
+        public void Test_Arena_Warriors_Fight_Missing_Deffender_Missing_Attacker(string missingWarrior)
+        {
+            this.arena.Enroll(this.warriorOne);
+            this.arena.Enroll(this.WarriorTwo);
+            this.arena.Enroll(this.WarriorThree);
+
+            Assert.That(() =>
+            {
+                this.arena.Fight(this.warriorOne.Name, missingWarrior);
+            }, Throws.InvalidOperationException.With.Message.EqualTo($"There is no fighter with name {missingWarrior} enrolled for the fights!"));
+
+            Assert.That(() =>
+            {
+                this.arena.Fight(missingWarrior, this.WarriorTwo.Name);
+            }, Throws.InvalidOperationException.With.Message.EqualTo($"There is no fighter with name {missingWarrior} enrolled for the fights!"));
+        }
+
+        [Test]
+        public void Test_Successful_Fight_With_Valid_Warriors()
+        {
+            this.arena.Enroll(this.warriorOne);
+            this.arena.Enroll(this.WarriorTwo);
+            this.arena.Enroll(this.WarriorThree);
+            this.arena.Fight(this.WarriorTwo.Name, this.warriorOne.Name);
+            Assert.That(this.warriorOne.HP, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Test_Not_Successful_Fight_Due_ToWeakAttacker()
+        {
+            this.arena.Enroll(this.warriorOne);
+            this.arena.Enroll(this.WarriorTwo);
+            this.arena.Enroll(this.WarriorThree);
+            Assert.That(() =>
+            {
+                this.arena.Fight(this.warriorOne.Name, this.WarriorTwo.Name);
+            },Throws.InvalidOperationException.With.Message.EqualTo($"You are trying to attack too strong enemy"));
+        }
+    }
+}
---
+++ Practic_2023/Fighting_Arena/FightingArena.Tests/WarriorTests.cs
@@ -0,0 +1,98 @@
+namespace FightingArena.Tests
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class WarriorTests
+    {
+
+        private Warrior testWarrior;
+
+        [TestCase("")]
+        [TestCase(null)]
+        public void Test_Ctor_Name(string name)
+        {
+            Assert.Throws<ArgumentException>(() => this.testWarrior = new Warrior(name, 100, 100));
+        }
+
+        [TestCase(0)]
+        [TestCase(-10)]
+        public void Test_Ctor_Damage(int damage)
+        {
+            Assert.Throws<ArgumentException>(() => this.testWarrior = new Warrior("Test", damage, 100));
+        }
+
+        [TestCase(-10)]
+        public void Test_Ctor_Hp(int hp)
+        {
+            Assert.Throws<ArgumentException>(() => this.testWarrior = new Warrior("Test", 100, hp));
+        }
+
+        [TestCase("Test", 10, 10)]
+        [TestCase("Test1", 1, 0)]
+        public void Test_Ctor_Success_Created(string name, int damage, int hp)
+        {
+            this.testWarrior = new Warrior(name, damage, hp);
+            Assert.That(testWarrior.Name, Is.EqualTo(name));
+            Assert.That(testWarrior.Damage, Is.EqualTo(damage));
+            Assert.That(testWarrior.HP, Is.EqualTo(hp));
+        }
+
+        [Test]
+        public void Test_Attack_LowHP()
+        {
+            this.testWarrior = new Warrior("Test", 100, 15);
+            Warrior attackedWarrior = new Warrior("Test1", 100, 100);
+
+            Assert.That(() =>
+            {
+                this.testWarrior.Attack(attackedWarrior);
+            }, Throws.InvalidOperationException.With.Message.EqualTo("Your HP is too low in order to attack other warriors!"));
+        }
+
+        [Test]
+        public void Test_Attack_Enemy_Low_HP()
+        {
+            this.testWarrior = new Warrior("Test", 100, 100);
+            Warrior attackedWarrior = new Warrior("Test1", 100, 15);
+
+            Assert.That(() =>
+            {
+                this.testWarrior.Attack(attackedWarrior);
+            }, Throws.InvalidOperationException.With.Message.EqualTo($"Enemy HP must be greater than 30 in order to attack him!"));
+        }
+
+        [Test]
+        public void Test_Attack_With_LowerHP_Than_Attacker_Damage()
+        {
+            this.testWarrior = new Warrior("Test", 100, 50);
+            Warrior attackedWarrior = new Warrior("Test1", 100, 100);
+
+            Assert.That(() =>
+            {
+                this.testWarrior.Attack(attackedWarrior);
+            }, Throws.InvalidOperationException.With.Message.EqualTo($"You are trying to attack too strong enemy"));
+        }
+
+        [Test]
+        [TestCase(100)]
+        [TestCase(150)]
+        public void Test_Successful_Attack_With_Highier_Damage(int damage)
+        {
+            this.testWarrior = new Warrior("Test", damage, 100);
+            Warrior attackedWarrior = new Warrior("Test1", 100, 100);
+            this.testWarrior.Attack(attackedWarrior);
+            Assert.That(attackedWarrior.HP, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Test_Successful_Attack()
+        {
+            this.testWarrior = new Warrior("Test", 50, 100);
+            Warrior attackedWarrior = new Warrior("Test1", 100, 50);
+            this.testWarrior.Attack(attackedWarrior);
+            Assert.That(attackedWarrior.HP, Is.EqualTo(0));
+        }
+    }
+}
